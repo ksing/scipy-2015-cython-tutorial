6587795b46cd2c437bca7b2424b86d8f6a4181be
Kushagra Singhal <kush@fairfrogdev.nl>
2018-10-18 00:14:53 +0200

    Solve hamming_python, update test

--- exercises/02-typing/01-hamming-distance/hamming_cython.pyx
+++ exercises/02-typing/01-hamming-distance/hamming_cython.pyx
@@ -1,12 +1,30 @@
-def hamming_sum(s0, s1):
-    if len(s0) != len(s1):
+def hamming_sum(char* s0, char* s1):
+    cdef:
+        int len_s0 = len(s0)
+        int len_s1 = len(s1)
+    if len_s0 != len_s1:
         raise ValueError()
-    return sum(c0 != c1 for (c0, c1) in zip(s0, s1))
+    return sum(get_count(c0, c1) for (c0, c1) in zip(s0, s1))
 
-def hamming_loop(s0, s1):
-    if len(s0) != len(s1):
+
+cdef int get_count(char c0, char c1):
+    cdef int inequality = c0 != c1
+    return inequality
+
+
+cpdef int hamming_loop(char* s0, char* s1):
+    cdef:
+        int len_s0 = len(s0)
+        int len_s1 = len(s1)
+    if len_s0 != len_s1:
         raise ValueError()
-    count = 0
-    for i in range(len(s0)):
-        count += (s0[i] != s1[i])
+    cdef:
+        int count = 0
+        int i
+        char s0_i
+        char s1_i
+    for i in range(len_s0):
+        s0_i = s0[i]
+        s1_i = s1[i]
+        count += s0_i != s1_i
     return count
